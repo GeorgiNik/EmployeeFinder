eb087d79e22d0c664e0c879f22d5022e4fbc9a51
GeorgiNik <georgi.nik95@gmail.com>
2016-01-26 16:13:17 +0200

    added repository

---
+++ EmployeeFinder.Models/User.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeFinder.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Security.Claims;
+
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    public class User : IdentityUser
+    {
+        
+
+        public User()
+        {
+           
+        }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(30)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(30)]
+        public string LastName { get; set; }
+
+        [Required]
+        public string AvatarUrl { get; set; }
+
+        [Required]
+        public int Rating { get; set; }
+
+       
+
+        public ClaimsIdentity GenerateUserIdentity(UserManager<User> manager)
+        {
+            //// Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = manager.CreateIdentity(this, DefaultAuthenticationTypes.ApplicationCookie);
+            //// Add custom user claims here
+            return userIdentity;
+        }
+
+        public Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<User> manager)
+        {
+            return Task.FromResult(GenerateUserIdentity(manager));
+        }
+    }
+}
